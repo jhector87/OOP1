265d101ca62d7bf28592f94eb30045f7eb3f288f
HectorGraphics® <jonathan@hectorgraphics.ch>
2017-09-19 11:22:36 +0200

    First OOP1 2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        String name = input.nextLine();
+        System.out.printf("Hello, %s!\nHow are you doing today?", name);
     }
 }
