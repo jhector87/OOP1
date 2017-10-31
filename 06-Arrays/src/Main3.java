4c1d51c9bea3d979b7641d73f2648f5409f4fffa
HectorGraphics® <jonathan@hectorgraphics.ch>
2017-10-31 12:55:02 +0100

    Arrays updated.

--- 06-Arrays/src/Main3.java
+++ 06-Arrays/src/Main3.java
@@ -1,16 +1,49 @@
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 public class Main3 {
+
+    private static Random rd = new Random();
 
     public static void main(String[] args) {
         final int ARRAY_LENGTH = 20, MAX_VALUE = 100;
         int[] values;
+
+        values = getDistinctValues(ARRAY_LENGTH, MAX_VALUE);
+        System.out.println(Arrays.toString(values));
+
         values = getDistinctValues(ARRAY_LENGTH, MAX_VALUE);
         System.out.println(Arrays.toString(values));
     }
 
     private static int[] getDistinctValues(int arrayLength, int maxValue) {
+        int[] array = new int[arrayLength];
+        int i = 0;
+        while (i < array.length) {
+            array[i] = rd.nextInt(maxValue) + 1;
+            boolean found = false;
+            for (int j = 0; j < i && !found; j++) {
+                if (array[j] == array[i])
+                    found = true;
+            }
+            if (!found)
+                i++;
 
+        }
+        return array;
+    }
+
+    private static int[] getDistinctValues2(int arrayLength, int maxValue) {
+        int[] companionArray = new int[maxValue];
+        for (int i = 0; i < maxValue; i++) {
+            companionArray[i] = i + 1;
+        }
+        int[] array = new int[arrayLength];
+        for(int i = 0; i < maxValue; i++) {
+
+        }
     }
 
 }
